b34620e908e5d2733d998d531e96ce2bf09dda30
LKSHAY123 <lakshaysharma4781@gmail.com>
2026-04-01 01:16:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const observer=new IntersectionObserver(entries=>{
-entries.forEach(entry=>{
-if(entry.isIntersecting){
-entry.target.style.opacity=1;
-entry.target.style.transform="translateY(0)";
-}});
-});
-
-document.querySelectorAll(".section,.card").forEach(el=>{
-el.style.opacity=0;
-el.style.transform="translateY(50px)";
-observer.observe(el);
-});
+function scrollToDonate(){
+  document.getElementById("donate").scrollIntoView({behavior:"smooth"});
+}
